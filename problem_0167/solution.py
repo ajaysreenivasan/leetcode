f417643c339cacb54c24e0ccff6d24da813d1f1e
Ajay <ajaysreenivasan@gmail.com>
2021-02-16 20:25:15 -0500

    New problem, and changed intuition formatting.

--- problem_0167/solution.py
+++ problem_0167/solution.py
@@ -1,8 +1,21 @@
 from typing import List
 
 class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(nums) - 1
 
+        temp = nums[i] + nums[j]
 
+        while(temp != target):
+            temp = nums[i] + nums[j]
+
+            if(temp < target):
+                i += 1
+            elif(temp > target):
+                j -= 1
+
+        return [i+1, j+1]
 
 def main():
     # THIS METHOD INTENTIONALLY LEFT EMPTY
